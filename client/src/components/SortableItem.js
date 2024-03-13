032f71aee4d81067966b070d6bf7fc936d756440
Jordan Bruce <jeb79@njit.edu>
2024-03-13 09:36:52 -0400

    Added a div for the settings section for each row.

--- client/src/components/SortableItem.js
+++ client/src/components/SortableItem.js
@@ -12,8 +12,10 @@ export function SortableItem(props) {
   } = useSortable({id: props.id});
   
   const style = {
+    position: 'relative',
     border: '1px solid',
     width: 'auto',
+    height: '90px',
     transform: CSS.Transform.toString(transform),
     transition,
   };
@@ -21,6 +23,18 @@ export function SortableItem(props) {
   return (
     <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
       {props.children}
+      <div className='settings-box'
+        style={{
+          position: 'absolute',
+          width: '60px',
+          height: '100%',
+          top: 0,
+          right: 0,
+          backgroundColor: 'grey',
+        }}
+      >
+        hello
+      </div>
     </div>
   );
 }
