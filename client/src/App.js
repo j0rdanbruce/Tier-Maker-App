bec3084be94b9299b6ab003621615cf8d340479a
Jordan Bruce <jeb79@njit.edu>
2024-03-21 11:24:48 -0400

    Added navigatio nbar component.

---
+++ client/src/App.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+import { socket } from './socket';
+import { ConnectionState } from './components/ConnectionState/ConnectionState';
+import { ConnectionManager } from './components/ConnectionManager/ConnectionManager';
+import SortableList from './components/SortableList';
+
+//Browser routing related imports go here
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+//Navigation Bar imports go here
+import NavigationBar from './components/NavigationBar/NavigationBar';
+
+function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('sort change event', (values) => {
+      document.getElementById('info').innerText = JSON.stringify(values);
+    });
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <NavigationBar />
+
+      <ConnectionState isConnected={isConnected} />
+      <ConnectionManager />
+      
+      <SortableList />
+      <p id='info'>Here</p>
+      <h2>Add Portion</h2>
+      <div>
+        <button>Add Item</button>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
