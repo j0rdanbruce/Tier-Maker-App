77ee56d271b2d677378ad3ac4e0ea768e2981188
Jordan Bruce <jeb79@njit.edu>
2024-03-21 10:38:26 -0400

    created the basic component for the ranking session. nothing much added yet.

---
+++ client/src/components/RankingSession/RankingSession.js
@@ -0,0 +1,10 @@
+/**
+ * The component that represents a ranking session that any user can join on. 
+ * Users on same session can create tier lists and rank items on same session.
+ */
+
+const RankingSession = (props) => {
+  
+}
+
+export default RankingSession;
